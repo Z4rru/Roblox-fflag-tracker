7afc58a6230c51403330e5846bb839071ff55dea
Z4rru <123763147+Z4rru@users.noreply.github.com>
2025-09-22 13:51:56 +0800

    Update updateff.py

--- updateff.py
+++ updateff.py
@@ -325,6 +325,7 @@ const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 const particles = Array.from({{length:80}}, () => {{x:Math.random()*canvas.width, y:Math.random()*canvas.height, r:Math.random()*2+1, dx:(Math.random()-0.5)/2, dy:(Math.random()-0.5)/2}});
+
 function animateParticles() {{
     ctx.clearRect(0,0,canvas.width,canvas.height);
     particles.forEach(p => {{
